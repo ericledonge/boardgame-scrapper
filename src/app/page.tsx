c166de1ff4639379e879b05d706dee3d50574dbb
Eric <eric.ledonge@gmail.com>
2023-11-04 22:14:31 -0400

    :lipstick: table (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,48 @@
-import { XMLParser } from "fast-xml-parser";
-import { z } from "zod";
-
-type Game = {
-  id: string;
-  name: string;
-  yearPublished: string;
-  thumbnail: string;
-};
-
-const GameSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  yearpublished: z.string(),
-  thumbnail: z.string(),
-});
-
-type GameSchemaTypes = z.infer<typeof GameSchema>;
-
-const BGG_URL =
-  "https://boardgamegeek.com/xmlapi2/collection?username=Empyrium&own=1";
-
-const parser = new XMLParser({
-  attributeNamePrefix: "@_",
-  ignoreAttributes: false,
-});
+import { getBggUserGames } from "@/services/get-bgg-user-games";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Game } from "@/models/game";
 
 export default async function Home() {
-  const response = await fetch(BGG_URL);
-  const text = await response.text();
-
-  const games = parser.parse(text).items.item.map((game: GameSchemaTypes) => ({
-    // @ts-ignore
-    id: game["@_objectid"],
-    // @ts-ignore
-    name: game.name["#text"],
-    yearPublished: game.yearpublished,
-    thumbnail: game.thumbnail,
-  }));
-
-  const orderedGames = games.sort((a: Game, b: Game) => {
-    const yearA = parseInt(a.yearPublished, 10);
-    const yearB = parseInt(b.yearPublished, 10);
-
-    if (yearB - yearA !== 0) {
-      return yearB - yearA;
-    } else {
-      return parseInt(b.id, 10) - parseInt(a.id, 10);
-    }
-  });
+  const games = await getBggUserGames({ name: "Empyrium" });
 
   return (
     <main>
-      <ul>
-        {orderedGames.map((game: Game) => (
-          <li key={game.name}>
-            <img src={game.thumbnail} alt={game.name} />
-            <h2>{game.name}</h2>
-            <p>{game.yearPublished}</p>
-            <a
-              href={`https://boardgamegeek.com/boardgame/${game.id}`}
-              target={"_blank"}
-            >
-              Link
-            </a>
-          </li>
-        ))}
-      </ul>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Image</TableHead>
+            <TableHead>Year Published</TableHead>
+            <TableHead>Link</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {games.map((game: Game) => (
+            <TableRow key={game.name}>
+              <TableCell>{game.name}</TableCell>
+              <TableCell>
+                <img src={game.thumbnail} alt={game.name} />
+              </TableCell>
+              <TableCell>{game.yearPublished}</TableCell>
+              <TableCell>
+                <a
+                  href={`https://boardgamegeek.com/boardgame/${game.id}`}
+                  target={"_blank"}
+                >
+                  Link
+                </a>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </main>
   );
 }
